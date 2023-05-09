185bf96558982a031aeaa6cd5db8225647fd7684
Rigoberto Obregon <robregon_ifds@TKH-M1-Air-24.lan>
2023-05-08 21:45:08 -0700

    Adding insert command.

---
+++ code/config2.py
@@ -0,0 +1,8 @@
+#Password="ds2022_aws"
+params2 = {
+    "host"      : "rds-pg-jobs.chfavwsr5bmp.us-east-1.rds.amazonaws.com",
+    "dbname"    : "postgres",
+    "user"      : "postgres",
+    "password"  : "ds2022_aws",
+    "port" : "5432"     
+}
