7690b563fde0e3642c9d2b72598ed9fedc599fc7
Anastasiya Ayala <anastasiyaselevich@gmail.com>
2023-05-09 13:39:12 -0400

    remove file

--- code/config2.py
+++
@@ -1,8 +0,0 @@
-#Password="ds2022_aws"
-params2 = {
-    "host"      : "rds-pg-jobs.chfavwsr5bmp.us-east-1.rds.amazonaws.com",
-    "dbname"    : "postgres",
-    "user"      : "postgres",
-    "password"  : "ds2022_aws",
-    "port" : "5432"     
-}
